7bd726ba7dc023dd74844bd2a7341af989a6c574
Fairbanks, Ben-CW <admin@marathonpets.com>
2018-12-09 12:42:36 -0600

    Folder structure and form setup

--- src/App.js
+++ src/App.js
@@ -1,28 +1,30 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Navbar from './components/nav-layout/navbar';
+import { Link } from './components/link-dashboard';
+import SignInCard from './components/link-cards/signin-card';
+import SignUpCard from './components/link-cards/signup-card';
+import ConnectCard from './components/link-cards/connect-card';
+import Dashboard from './components/dashboard';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<BrowserRouter>
+				<div className='App'>
+					<Navbar />
+					<Switch>
+         				<Route exact path="/" component={Dashboard} />
+         				<Route path="/link" component={Link} />
+         				<Route path="/signin" component={SignInCard} />
+         				<Route path="/signup" component={SignUpCard} />
+         				<Route path="/connect" component={ConnectCard} />
+         				<Route path="/dashboard/:id" component={Dashboard} />
+					</Switch>
+				</div>
+			</BrowserRouter>
+		);
+	}
 }
 
 export default App;
